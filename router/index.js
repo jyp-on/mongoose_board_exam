2b6fb39df55b9efad870182a70504ccd76d0e6c0
mythpoy <okmlnsunok@gmail.com>
2022-07-06 15:35:56 +0900

    삭제 구현

--- router/index.js
+++ router/index.js
@@ -13,25 +13,57 @@ router.get('/', function(요청, 응답){
 })
 
 /* Write board page */
-router.get('/write', function(요청, 응답) {
+router.get('/board/write', function(요청, 응답) {
   응답.render('write');
 });
 
 /* board insert mongo */
 router.post('/board/write', function (요청, 응답) {
-let board = new Board();
-board.title = 요청.body.title;
-board.contents = 요청.body.contents;
-board.author = 요청.body.author;
+  let board = new Board();
+  board.title = 요청.body.title;
+  board.contents = 요청.body.contents;
+  board.author = 요청.body.author;
 
-board.save(function (err) {
-  if(err){
-    console.log(err);
+  board.save(function (에러) {
+    if(에러){
+      console.log(에러);
+      응답.redirect('/');
+    }
     응답.redirect('/');
-  }
-  응답.redirect('/');
+  });
 });
+
+router.delete('/board/delete',  function(요청, 응답){
+   Board.deleteOne({_id:요청.body.id}, 
+    function(에러){
+    if(에러){
+      console.log(에러)
+    }
+    응답.redirect('/');
+  })
 });
+
+router.get('/board/edit/:id', function(요청, 응답){
+  Board.findOne({_id:요청.params.id}, function(에러, 결과){
+    if(에러){console.log(에러)}
+    응답.render("edit", 결과);
+  })
+})
+
+router.put('/board/edit', function(요청, 읃답){
+  const contents = 요청.body.contents
+  const title = 요청.body.title
+  Board.findOneAndUpdate({_id:요청.body.id}, 
+    { $set: {
+      contents : contents,
+      title : title
+    }}, function(에러, 결과){
+      if(에러){
+        console.log(에러);
+        응답.redirect('/');
+      }
+    })
+})
 
 /* board find by id */
 router.get('/detail/:id', function (요청, 응답) {
@@ -43,17 +75,17 @@ router.get('/detail/:id', function (요청, 응답) {
 
 /* comment insert mongo*/
 router.post('/comment/write', function (요청, 응답){
-  var comment = new Comment();
+  let comment = new Comment();
   comment.contents = 요청.body.contents;
   comment.author = 요청.body.author;
-
+  console.log(comment)
   Board.findOneAndUpdate({_id : 요청.body.id}, 
-    { $push: { comments : comment}}, function (err, board) {
-      if(err){
-          console.log(err);
+    { $push: { comments : comment}}, function (에러, 결과) {
+      if(에러){
+          console.log(에러);
           응답.redirect('/');
       }
-      응답.redirect('/board/'+요청.body.id);
+      응답.redirect('/detail/'+요청.body.id);
   });
 });
 
