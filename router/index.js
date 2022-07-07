690e0005cfd6a9db9f1acfaa4eaae37ba06a4dd7
mythpoy <okmlnsunok@gmail.com>
2022-07-07 14:28:45 +0900

    로그인 전 후 nav바 변경

--- router/index.js
+++ router/index.js
@@ -13,7 +13,17 @@ router.use(session({secret : '비밀코드', resave : true, saveUninitialized: f
 router.use(passport.initialize());
 router.use(passport.session());
 
+router.use(function(req,res,next){
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  next();
+});
+
 /*회원가입 */
+router.get('/register', (요청, 응답)=>{
+  응답.render('register');
+})
+
 router.post("/register", (요청, 응답)=>{
   User.findOne({email:요청.body.email}).then((user)=>{
     if(user){ //이미 있는 유저면
@@ -34,20 +44,23 @@ router.post("/register", (요청, 응답)=>{
 })
 
 /*로그인 */
+router.get('/login', (요청, 응답)=>{
+  응답.render('login', {msg:1});
+})
+
 router.post('/login', passport.authenticate('local', {
   failureRedirect : '/fail'
 }),function(요청, 응답){
-  console.log(요청.body);
   응답.redirect('/')
 });
 
 router.get('/fail', function(요청, 응답){
-  응답.send('로그인 실패..')
+  응답.render('login', {msg:'로그인 실패'})
 })
 
 passport.use(new LocalStrategy({ //인증하는 방법
   usernameField: 'email', //input의 name이 뭔지.
-  passwordField: 'pw',
+  passwordField: 'password',
   session: true,
   passReqToCallback: false, //true로 바꾸면 fucntion에 req를 넣어서 아디, 비번 외에 정보를 받기 가능
 }, function (입력한이메일, 입력한비번, done) {
@@ -55,18 +68,18 @@ passport.use(new LocalStrategy({ //인증하는 방법
     if (에러) return done(에러)
 
     //일치하는 id가 없으면
-    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (!결과) return done(null, false, { msg: '존재하지않는 아이디입니다.' })
     //일치하는 아디가 있고 패스워드도 같으면
     if (입력한비번 == 결과.password) {
       return done(null, 결과) //결과를 담아서 serializeUser로 넘김
     } else {
-      return done(null, false, { message: '비번틀렸어요' })
+      return done(null, false, { msg: '비번틀렸어요' })
     }
   })
 }));
 
 passport.serializeUser(function(user, done){
-  done(null, user.id); //id를 이용해서 세션을 저장시키는 코드
+  done(null, user.email); //id를 이용해서 세션을 저장시키는 코드
 });
 
 passport.deserializeUser(function(아이디, done){
@@ -84,6 +97,11 @@ function 로그인여부(요청, 응답, next){
   }
 }
 
+/* 로그아웃 */
+router.get('/logout', function(요청, 응답) {
+  요청.logout();
+  응답.redirect('/');
+});
 
 /* index -> 게시판 보여주는 곳 */
 router.get('/', function(요청, 응답){
